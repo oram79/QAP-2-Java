07e9561d13bdb8f76303e00087624760f833f149
Logan Oram <logan.oram@keyin.com>
2024-10-04 15:58:40 -0230

    MyLine 1/2 complete
    still working on fixing Mypoint error

--- Question1/MyLine.java
+++ Question1/MyLine.java
@@ -1,5 +1,28 @@
 package Question1;
 
 public class MyLine {
-    
+    private MyPoint begin;
+    private MyPoint end;
+
+    public MyLine(MyPoint begin, MyPoint end) {
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public Mypoint getBegin() {
+        return begin;
+    }
+
+    public Mypoint getEnd() {
+        return end;
+    }
+
+    public double getLength() {
+        return begin.distance(end);
+    }
+
+    public String toString() {
+        return "Line From " + begin + " To " + end;
+    }
 }
+
