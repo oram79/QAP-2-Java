65f6ac8884efda4e08e54d48db109e4de8fc55f1
Logan Oram <logan.oram@keyin.com>
2024-10-05 21:03:38 -0230

    question 2 update

--- Question2/MyRectangle.java
+++ Question2/MyRectangle.java
@@ -1,5 +1,21 @@
 package Question2;
+import Question1.MyPoint;
 
 public class MyRectangle {
+    private MyPoint topLeft;
+    private MyPoint bottomRight;
+
+    public MyRectangle(MyPoint topLeft , MyPoint bottomRight) {
+        this.topLeft = topLeft;
+    }
+
+    public double getWidth() {
+        return Math.abs(bottomRight.getX() - topLeft.getX());
+    }
+    public double getHeight() {
+        return Math.abs(bottomRight.getY() - topLeft.getY());
+    }
     
 }
+
+
