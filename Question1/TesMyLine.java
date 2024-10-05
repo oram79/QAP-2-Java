3b81d06185a6e0789f99c535d4293f9e81af4e42
Logan Oram <logan.oram@keyin.com>
2024-10-04 23:29:38 -0230

    Test is complete
    Run multiple times with different x & y  (Works perfect)

--- Question1/TesMyLine.java
+++ Question1/TesMyLine.java
@@ -1,5 +1,12 @@
 package Question1;
 
 public class TesMyLine {
-    
+    public static void main(String[] args) {
+        MyPoint p1 = new MyPoint(3, 7);
+        MyPoint p2 = new MyPoint(4, 5);
+        MyLine line = new MyLine(p1, p2);
+
+        System.out.println(line);
+        System.out.println("Length: " + line.getLength());
+    }
 }
