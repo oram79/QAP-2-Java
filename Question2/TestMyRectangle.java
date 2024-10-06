63734872739e6ad959d306b361362284f2fd8517
Logan Oram <logan.oram@keyin.com>
2024-10-06 20:10:58 -0230

    test file has errors
    when running the file "bottomRight" is Null,, need to fix

--- Question2/TestMyRectangle.java
+++ Question2/TestMyRectangle.java
@@ -1,5 +1,15 @@
 package Question2;
 
+import Question1.MyPoint;
+
 public class TestMyRectangle {
-    
+    public static void main(String[] args) {
+    MyPoint topLeft = new MyPoint(1, 5);
+    MyPoint bottomRight = new MyPoint(4, 1);
+
+        MyRectangle rectangle = new MyRectangle(topLeft, bottomRight);
+
+        System.out.println(rectangle);
+        System.out.println(" The Area is: " + rectangle.getArea());
+    }
 }
