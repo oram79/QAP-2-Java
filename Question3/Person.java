a784fb66d96e28ce06acc0c875f3049681b93a61
Logan Oram <logan.oram@keyin.com>
2024-10-07 14:34:05 -0230

    person complete

--- Question3/Person.java
+++ Question3/Person.java
@@ -1,5 +1,17 @@
 package Question3;
 
 public class Person {
-    
+    private String firstName;
+    private String lastName;
+    private Address address;
+
+    public Person(String firstName, String lastName, Address address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+    public String toString() {
+        return lastName + " " + firstName + "," + address;
+    }
 }
