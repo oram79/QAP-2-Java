02b41acaded9ebbb3b267c5604883cdeb716705a
Logan Oram <logan.oram@keyin.com>
2024-10-07 14:26:50 -0230

    address done

--- Question3/Address.java
+++ Question3/Address.java
@@ -1,5 +1,19 @@
 package Question3;
 
 public class Address {
-    
+    private String street;
+    private String city;
+    private String state;
+    private String zipcode;
+
+    public Address(String street, String city, String state, String zipcode) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
+    }
+
+    public String toString() {
+        return street + "," + city + "," + state + zipcode;
+    }
 }
